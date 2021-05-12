6e29e370ca06a2b4d375131ff6ab3836da9120bc
Divya K <kdivyamar@gmail.com>
2021-05-12 17:08:26 +0530

    Registration-Feature1

--- src/main/java/in/divya/servlet/RegisterActionServlet.java
+++ src/main/java/in/divya/servlet/RegisterActionServlet.java
@@ -26,38 +26,45 @@ public class RegisterActionServlet extends HttpServlet {
 		/**
 		 * Step 1: Get Form Values
 		 */
-		StudentDetails student=new StudentDetails();
-		student.setStudentName(request.getParameter("studentName"));
-		student.setFatherName(request.getParameter("fatherName"));
-		student.setMotherName(request.getParameter("motherName"));
-		student.setStudentEmailId(request.getParameter("emailId"));
-		student.setStudentPassword(request.getParameter("password"));
-		student.setStudentRollNumber(request.getParameter("rollNumber"));
-		student.setGender(request.getParameter("gender"));
-		student.setStudentAddress(request.getParameter("address"));
-		student.setStudentCity(request.getParameter("city"));
-		student.setOccupation(request.getParameter("occupation"));
-		student.setStudentBloodGroup(request.getParameter("bloodGroup"));
-		student.setStudentStandard(request.getParameter("standard"));
-		student.setParentMobileNumber(Long.parseLong(request.getParameter("mobileNumber")));
-		student.setDateOfBirth(LocalDate.parse(request.getParameter("dateOfBirth")));
-		student.setDateOfJoining(LocalDate.parse(request.getParameter("dateOfJoining")));
-		/**
-		 * Step 2: Call Service => add student details
-		 */
-		boolean isAddedStudent = AddStudentDetails.addStudent(student);
-		/**
-		 * Step 3: Decide to which page we should redirect ?
-		 */
-		if(isAddedStudent){
-			String message = "SUCCESSFULLY REGISTERED";
-			response.sendRedirect("Register.jsp?infoMessage="+message);
-			
-		}
-		else{
-			String message = "Registration Failed";
-			response.sendRedirect("Register.jsp?errorMessage="+message);
-		}
+    	try
+    	{
+			StudentDetails student=new StudentDetails();
+			student.setStudentName(request.getParameter("studentName"));
+			student.setFatherName(request.getParameter("fatherName"));
+			student.setMotherName(request.getParameter("motherName"));
+			student.setStudentEmailId(request.getParameter("emailId"));
+			student.setStudentPassword(request.getParameter("password"));
+			student.setStudentRollNumber(request.getParameter("rollNumber"));
+			student.setGender(request.getParameter("gender"));
+			student.setStudentAddress(request.getParameter("address"));
+			student.setStudentCity(request.getParameter("city"));
+			student.setOccupation(request.getParameter("occupation"));
+			student.setStudentBloodGroup(request.getParameter("bloodGroup"));
+			student.setStudentStandard(request.getParameter("standard"));
+			student.setParentMobileNumber(Long.parseLong(request.getParameter("mobileNumber")));
+			student.setDateOfBirth(LocalDate.parse(request.getParameter("dateOfBirth")));
+			student.setDateOfJoining(LocalDate.parse(request.getParameter("dateOfJoining")));
+			/**
+			 * Step 2: Call Service => add student details
+			 */
+			boolean isAddedStudent = AddStudentDetails.addStudent(student);
+			/**
+			 * Step 3: Decide to which page we should redirect ?
+			 */
+			if(isAddedStudent){
+				String message = "SUCCESSFULLY REGISTERED";
+				response.sendRedirect("Register.jsp?infoMessage="+message);
+				
+			}
+			else{
+				String message = "Registration Failed";
+				response.sendRedirect("Register.jsp?errorMessage="+message);
+			}
+    	}
+    	catch (Exception e) {
+ 			String message = e.getMessage();
+ 			response.sendRedirect("Register.jsp?errorMessage=" + message);
+ 		}
 		
 	
 	}
